f83419ce7b6c350f556dc288f5a9c69f02ec180a
hd-code <54773293+hd-code@users.noreply.github.com>
2021-06-25 11:12:37 +0200

    added api test in dev html

--- backend/frontend/main.js
+++ backend/frontend/main.js
@@ -1,1 +1,12 @@
-alert("File server works");
+document.getElementById("hello-from-js").innerText =
+  "Hello from external JS file as well";
+
+const responseField = document.getElementById("response-field");
+
+function sendRequest(url) {
+  fetch(url).then((res) => {
+    res.json().then((json) => {
+      responseField.innerHTML = JSON.stringify(json, null, "  ");
+    });
+  });
+}
